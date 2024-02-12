5051f3acb8ee56f9757041e5dc134394781a9862
bsilva-c <54143084+BrunoCostaGH@users.noreply.github.com>
2024-02-12 21:49:16 +0000

    FT-74 Develop Backend using Django
    feat: Add Django Rest Framework (DRF) to the project
    feat(/api/users): Add UserSerializer and CreateUserSerializer to serializers.py
    feat(/api/users): Add /api/users endpoint to list and create users
    feat(/api/users): Add UserView and CreateUserView to views.py

--- app/api/views.py
+++ app/api/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer, CreateUserSerializer
+
 
 # Create your views here.
+
+class UserView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CreateUserSerializer
+        return self.serializer_class
+
+
+class CreateUserView(APIView):
+    serializer_class = CreateUserSerializer
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            username = serializer.data.get('username')
+            password = make_password(serializer.data.get('password'))
+
+            user = User(username=username, password=password)
+            user.save()
+
+            return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_409_CONFLICT)
