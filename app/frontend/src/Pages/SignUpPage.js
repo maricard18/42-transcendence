8236422eab72533edeb291034b73c00c89fa5786
Mario Henriques <maricard@student.42porto.com>
2024-03-30 14:40:16 +0000

    feat: added h1 tag to sign-up page

--- app/frontend/src/Pages/SignUpPage.js
+++ app/frontend/src/Pages/SignUpPage.js
@@ -30,6 +30,7 @@ export default function SignUpPage() {
         <div className="container">
             <div className="center">
                 <div className="d-flex flex-column justify-content-center">
+					<h1 className="header mb-5">Sign Up</h1>
                     <form>
                         <div className="position-relative">
                             {errors && (
