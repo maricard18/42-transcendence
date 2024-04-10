b9aca033a6b588562361a847c6b7405cfcf374a3
Mario Henriques <maricard@student.42porto.com>
2024-04-10 21:41:21 +0100

    feat: changed auth verification for login

--- app/frontend/src/functions/authenticationErrors.js
+++ app/frontend/src/functions/authenticationErrors.js
@@ -3,7 +3,7 @@ export default async function handleResponse(response, formData, setFormData) {
     const jsonData = await response.json();
     const responseMessage = jsonData["errors"]["message"];
 
-    if (response.status == 400) {
+    if (response.status === 400) {
         if (jsonData["errors"]["username"][0] === "A user with that username already exists.") {
             errors.message = "This username already exists";
             errors.username = 1;
@@ -16,7 +16,7 @@ export default async function handleResponse(response, formData, setFormData) {
         errors.message = "Incorrect password";
         errors.password = 1;
         setFormData({ ...formData, password: "" });
-    } else if (responseMessage === "User Not Found") {
+    } else if (response.status === 404) {
         errors.message = "Username doesn't exist";
         errors.username = 1;
         setFormData({ ...formData, username: "", password: "" });
