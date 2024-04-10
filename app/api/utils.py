be64dcdec9ebce5af6604ebdf5187eb691e9f56d
Bruno Costa <54143084+BrunoCostaGH@users.noreply.github.com>
2024-04-10 15:51:14 +0100

    Refactor backend (#8)
    * FT-116 Implement Two-Factor Authentication (2FA)
    
    feat: add pyotp to requirements.txt
    feat: Implement time-based OTP, to work with external applications such as Google Authenticator. Works via the endpoint /api/otp.
    Signed-off-by: Bruno Costa <54143084+BrunoCostaGH@users.noreply.github.com>
    
    * FT-116 Implement Two-Factor Authentication (2FA)
    
    fix: add missing get_user function
    refactor: optimize imports
    Signed-off-by: Bruno Costa <54143084+BrunoCostaGH@users.noreply.github.com>
    
    * FT-124 Integrate Local Account Linking with 42 Authentication
    
    feat: Integrate local account linking with 42 authentication.
    refactor(/api/sso): clean up
    
    Signed-off-by: Bruno Costa <54143084+BrunoCostaGH@users.noreply.github.com>
    
    * FT-124 Integrate Local Account Linking with 42 Authentication
    
    fix: add missing condition, if user doesn't exist
    refactor: change variable names that could be mistaken for readability
    Signed-off-by: Bruno Costa <54143084+BrunoCostaGH@users.noreply.github.com>
    
    * FT-126 Fix Unable To Set Avatar
    
    fix: fix unable to set avatar, if user was created without avatar
    refactor: change variable names that could be mistaken for readability
    Signed-off-by: Bruno Costa <54143084+BrunoCostaGH@users.noreply.github.com>
    
    * Refactor Backend
    
    refactor: rename /app/ws directory to /app/websocket
    refactor!: remove unused django admin app, and middleware
    refactor: remove /api/users/:username for security reasons
    
    Signed-off-by: Bruno Costa <54143084+BrunoCostaGH@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Bruno Costa <54143084+BrunoCostaGH@users.noreply.github.com>

--- app/api/utils.py
+++ app/api/utils.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.views import exception_handler
 
@@ -15,20 +14,6 @@ def api_exception_handler(exc, context):
 
         return Response({'errors': {'message': response.data.get('detail'), 'code': response.status_code}},
                         status=response.status_code)
-
-
-def get_user(pk):
-    try:
-        user = User.objects.get(username=pk)
-    except User.DoesNotExist:
-        try:
-            if pk.isdigit():
-                user = User.objects.get(pk=pk)
-            else:
-                raise User.DoesNotExist
-        except User.DoesNotExist:
-            return None
-    return user
 
 
 def get_host(request):
