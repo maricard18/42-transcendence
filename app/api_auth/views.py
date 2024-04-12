acc2dbd62bb10c71bc59a56b395b9691de70f130
Bruno Costa <54143084+BrunoCostaGH@users.noreply.github.com>
2024-04-12 17:39:13 +0100

    Backend Refactor (#11)
    * feat: added gameOver variable to pong game
    fix: removes commas from App.js
    fix: deleted utils file and removed all logError and log Functions
    
    * fix: refactored pong game logic
    
    - created a new Game Class to avoid duplicate code
    - added a switch case for a cleaner and more readable code
    - deleted Cpu and Opponent files, now they are inside the Player2 file
    - inside the PongGame there are always 2 players (Player1 and Player2); Player2 can be an Opponent or a CPU (both inherit from the Player base class)
    - in multiplayer the Opponent now has a ball too; the ball is updated through host messages and user prediction
    - added draw functions when the Opponent game is paused
    
    * fix: canvas is now responsible
    fix: changed all the constant variables to be scalar to the screen size
    feat: added a start game animation
    feat: added a new Context for in-game users, to avoid unnecessary fetches
    
    * fix: fixed host wrong score at creation
    fiz: refactored Class logic for Game, Player, Cpu and Opponent
    
    * FT-114 Live Pong Game Bugs
    
    fix(pong.js): add import to fix, uncaught ReferenceError exception
    
    * feat: added 42 login link
    feat: created frontend endpoint to get env vars
    fix: refactored some code
    
    * feat: added 409 confict support do sign up with 42
    feat: added automatic avatar loader when 42 users needs to choose a new username
    
    * feat: removed unecessary fetches
    feat: stored evry value changed or created in userInfo Context
    
    * fiz: fixed incorrect game ending
    del: deleted Loading Context
    
    * feat: merged 114
    
    * fix: fixed game never ending for opponent if user leaves mid game
    
    * fix: deleted userData state when user leaves the waiting room or game
    feat: added min and max for width and height
    
    * fix: fixed in game refreshed and page navigation
    
    * feat: added action
    
    * fix: fixed double import in NavigatioBar
    
    * FT-110 Sign-up with 42
    
    fix: add missing import
    
    * Backend Refactor
    
    feat: make /auth/ into its own app
    fix: fix channels redis not having the correct name in requirements.txt
    refactor!: rename endpoint /api/otp to /api/users/:id/otp
    refactor!: rename endpoint /api/tokens to /auth/token
    refactor!: rename endpoint /api/sso to /auth/sso
    
    Fixes: FT-130
    Signed-off-by: Bruno Costa <54143084+BrunoCostaGH@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Bruno Costa <54143084+BrunoCostaGH@users.noreply.github.com>
    Co-authored-by: Mario Henriques <maricard@student.42porto.com>
    Co-authored-by: Mário Henriques <111349815+maricard18@users.noreply.github.com>

---
+++ app/api_auth/views.py
@@ -0,0 +1,197 @@
+import os
+
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from rest_framework import status, viewsets, serializers
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
+
+from .models import SSO_User
+from .permissions import TokenPermission, SSOPermission
+from .serializers import AuthUserSerializer, APITokenObtainPairSerializer, TokenSerializer
+
+
+#######################
+##### /auth/token #####
+#######################
+
+class TokenViewSet(viewsets.ViewSet):
+    permission_classes = [TokenPermission]
+
+    @staticmethod
+    def new_token(request):
+        serializer = AuthUserSerializer(data=request.data, partial=True)
+        if serializer.is_valid():
+            username = serializer.validated_data.get('username')
+            email = serializer.validated_data.get('email')
+            try:
+                if username:
+                    user = User.objects.get(username=username)
+                elif email:
+                    user = User.objects.get(email=email)
+                else:
+                    return Response({
+                        'errors': {
+                            'message': "Field 'username' or 'email' is required",
+                            'code': status.HTTP_400_BAD_REQUEST
+                        }
+                    }, status=status.HTTP_400_BAD_REQUEST)
+            except User.DoesNotExist:
+                return Response({
+                    'errors': {
+                        'message': 'Resource Not Found',
+                        'code': status.HTTP_404_NOT_FOUND
+                    }
+                }, status=status.HTTP_404_NOT_FOUND)
+            user = authenticate(username=AuthUserSerializer(user).data.get('username'),
+                                password=serializer.validated_data.get('password'))
+            if user is not None:
+                refresh_token = APITokenObtainPairSerializer.get_token(user)
+
+                return Response({
+                    'access_token': str(refresh_token.access_token),
+                    'token_type': "Bearer",
+                    'expires_in': refresh_token.access_token.lifetime.seconds,
+                    'refresh_token': str(refresh_token)
+                }, status=status.HTTP_200_OK)
+            return Response({
+                'errors': {
+                    'message': 'Unauthorized',
+                    'code': status.HTTP_401_UNAUTHORIZED
+                }
+            }, status=status.HTTP_401_UNAUTHORIZED)
+        return Response({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    @staticmethod
+    def refresh_token(request):
+        try:
+            refresh_token = RefreshToken(request.data.get('refresh_token'))
+        except TokenError as e:
+            return Response({
+                'errors': {
+                    'message': e.args[0],
+                    'code': status.HTTP_400_BAD_REQUEST
+                }
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({
+            'access_token': str(refresh_token.access_token),
+            'token_type': "Bearer",
+            'expires_in': refresh_token.access_token.lifetime.seconds,
+            'refresh_token': str(refresh_token)
+        }, status=status.HTTP_200_OK)
+
+    # POST /auth/token
+    def create(self, request):
+        serializer = TokenSerializer(data=request.data)
+        if serializer.is_valid():
+            if request.data.get('grant_type') == 'password':
+                return self.new_token(request)
+            elif request.data.get('grant_type') == 'refresh_token':
+                return self.refresh_token(request)
+        return Response({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+#######################
+#####  /auth/sso  #####
+#######################
+
+class SSOViewSet(viewsets.ViewSet):
+    permission_classes = [SSOPermission]
+
+    @classmethod
+    def check_params(cls, **kwargs):
+        action = kwargs.get("action")
+        user_id = kwargs.get("user_id")
+        code = kwargs.get("code")
+
+        if not action or not code:
+            raise serializers.ValidationError({
+                'errors': {
+                    'message': "'action' and 'code' query parameters are required",
+                    'code': status.HTTP_400_BAD_REQUEST
+                }
+            })
+
+        if action not in ['link', 'register']:
+            raise serializers.ValidationError({
+                'errors': {
+                    'message': "'action' query parameter must be 'link' or 'register'",
+                    'code': status.HTTP_400_BAD_REQUEST
+                }
+            })
+
+        if action == 'link' and not user_id:
+            raise serializers.ValidationError({
+                'errors': {
+                    'message': "'user_id' query parameter is required when linking",
+                    'code': status.HTTP_400_BAD_REQUEST
+                }
+            })
+
+    @classmethod
+    def sso_101010(cls, request):
+        auth_user = None
+        action = request.GET.get('action', None)
+        user_id = request.GET.get('user_id', None)
+        code = request.GET.get('code', None)
+
+        try:
+            cls.check_params(action=action, user_id=user_id, code=code)
+            if action == 'link':
+                auth_user = User.objects.get(pk=user_id)
+        except serializers.ValidationError as e:
+            return Response(e.detail, status=status.HTTP_400_BAD_REQUEST)
+        except User.DoesNotExist:
+            return Response({
+                'errors': {
+                    'message': 'Resource Not Found',
+                    'code': status.HTTP_404_NOT_FOUND
+                }
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        import requests
+
+        response = requests.post('https://api.intra.42.fr/oauth/token', data={
+            'grant_type': 'authorization_code',
+            'client_id': os.environ.get('42_CLIENT_ID'),
+            'client_secret': os.environ.get('42_CLIENT_SECRET'),
+            'code': code,
+            'redirect_uri': os.environ.get('42_REDIRECT_URI')
+        })
+        if response.status_code == 200:
+            user_info = requests.get('https://api.intra.42.fr/v2/me',
+                                     headers={'Authorization': 'Bearer ' + response.json()['access_token']}).json()
+            user, created, conflict = SSO_User.objects.get_or_create(
+                action=action,
+                auth_user=auth_user,
+                sso_provider='101010',
+                sso_id=user_info.get('id'),
+                username=user_info.get('login'),
+                email=user_info.get('email'),
+                image_link=user_info.get('image').get('link')
+            )
+
+            if action == 'link':
+                return Response({}, status=status.HTTP_201_CREATED)
+            else:
+                refresh_token = APITokenObtainPairSerializer.get_token(user)
+
+                return Response({
+                    'access_token': str(refresh_token.access_token),
+                    'token_type': "Bearer",
+                    'expires_in': refresh_token.access_token.lifetime.seconds,
+                    'refresh_token': str(refresh_token)
+                }, status=status.HTTP_200_OK if conflict is False else status.HTTP_409_CONFLICT)
+        return Response(response.json(), status=response.status_code)
+
+    def callback(self, request, pk=None):
+        if str(pk) == '101010':
+            return self.sso_101010(request)
+        else:
+            return Response({
+                'errors': {
+                    'message': 'Unauthorized',
+                    'code': status.HTTP_401_UNAUTHORIZED
+                }
+            }, status=status.HTTP_401_UNAUTHORIZED)
